23e774a8b2c6183a17d15e69e43f657b32f25462
Maria Rahmouni <rahmounimaria02@gmail.com>
2024-07-15 23:05:42 -0400

    created barebones pages for links from navbar

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/navigation-bar/NavBar";
+
+const Profile = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>Hello, I am the Profile page.</h1>
+        </div>
+    );
+};
+
+export default Profile;
